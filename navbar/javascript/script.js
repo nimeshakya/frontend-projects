df6cac232d820ad0fa59e8bf6a670c8c07dce634
nimeshakya <nimeshakyabtw@gmail.com>
2023-07-16 21:57:47 +0545

    display, position and transition after scrolling done

--- navbar/javascript/script.js
+++ navbar/javascript/script.js
@@ -2,20 +2,25 @@ const unscrolledNav = document.getElementById('unscrolled-navbar');
 const scrolledNav = document.getElementById('scrolled-navbar');
 
 window.addEventListener('scroll', () => {
+    let stuck = false;
     let scrollY = window.scrollY;
     scrollY = Math.round(scrollY);
     console.log(scrollY);
-    if (scrollY >= 190) {
-        unscrolledNav.classList.remove('nav-visible');
-        unscrolledNav.classList.add('nav-invisible');
+    if (!stuck && scrollY >= 190) {
+        unscrolledNav.classList.replace('nav-visible', 'nav-invisible');
 
-        scrolledNav.classList.remove('nav-invisible');
-        scrolledNav.classList.add('nav-visible');
+        scrolledNav.classList.replace(
+            'scrolled-nav-hidden',
+            'scrolled-nav-visible'
+        );
+        stuck = true;
     } else {
-        unscrolledNav.classList.remove('nav-invisible');
-        unscrolledNav.classList.add('nav-visible');
+        unscrolledNav.classList.replace('nav-invisible', 'nav-visible');
 
-        scrolledNav.classList.remove('nav-visible');
-        scrolledNav.classList.add('nav-invisible');
+        scrolledNav.classList.replace(
+            'scrolled-nav-visible',
+            'scrolled-nav-hidden'
+        );
+        stuck = false;
     }
 });
