89c5806716ecdf96bb5026d078040bed146a031b
nimeshakya <nimeshakyabtw@gmail.com>
2023-07-27 16:30:14 +0545

    scroll to specific part of page buttons done

--- landing/javascript/script.js
+++ landing/javascript/script.js
@@ -1,1 +1,9 @@
-console.log('hello, world!');
+const scrollTopBtn = document.getElementById('scroll-top-btn');
+
+const scrollTop = () => {
+    console.log('hello');
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+scrollTopBtn.addEventListener('click', scrollTop);
