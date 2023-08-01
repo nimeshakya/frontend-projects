43313b6eb3536f06f1ec2bbfbfe5e701aea31979
nimeshakya <nimeshakyabtw@gmail.com>
2023-08-01 17:47:33 +0545

    added: transition of objects while scrolling down

--- landing/javascript/script.js
+++ landing/javascript/script.js
@@ -1,9 +1,41 @@
 const scrollTopBtn = document.getElementById('scroll-top-btn');
-
+const scrollInvisibleItems = document.querySelectorAll('.object-invisible');
 const scrollTop = () => {
     console.log('hello');
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 };
 
+const windowScrollEvents = () => {
+    let scrollTop = document.documentElement.scrollTop;
+    if (scrollTop >= 180) {
+        console.log(document.getElementById('scroll-down-btn'));
+        document
+            .getElementById('scroll-down-btn')
+            .classList.replace('down-arrow-invisible', 'down-arrow-visible');
+    }
+    if (scrollTop >= 900) {
+        scrollInvisibleItems.forEach((elem, index) => {
+            if (index == 0 || index == 1) {
+                elem.classList.replace('object-invisible', 'object-visible');
+            }
+        });
+    }
+    if (scrollTop >= 1000) {
+        scrollInvisibleItems.forEach((elem, index) => {
+            if (index == 2 || index == 3 || index == 4) {
+                elem.classList.replace('object-invisible', 'object-visible');
+            }
+        });
+    }
+    if (scrollTop >= 1200) {
+        scrollInvisibleItems.forEach((elem, index) => {
+            if (index == 5) {
+                elem.classList.replace('object-invisible', 'object-visible');
+            }
+        });
+    }
+};
+
 scrollTopBtn.addEventListener('click', scrollTop);
+window.addEventListener('scroll', windowScrollEvents);
