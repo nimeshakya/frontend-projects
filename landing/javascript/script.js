48ef71ccf8b3d9bb6d7ae921e9afbf4dfea86da1
nimeshakya <nimeshakyabtw@gmail.com>
2023-08-03 16:08:13 +0545

    all UI working fine across various screen sizes

--- landing/javascript/script.js
+++ landing/javascript/script.js
@@ -1,6 +1,13 @@
 const scrollTopBtn = document.getElementById('scroll-top-btn');
 const scrollDownBtn = document.getElementById('scroll-down-btn');
 const scrollInvisibleItems = document.querySelectorAll('.object-invisible');
+
+// navigation buttons
+const siteNavigation = document.getElementById('site-navigation');
+const navOpenBtn = document.getElementById('nav-open-btn');
+const navCloseBtn = document.getElementById('nav-close-btn');
+const navigationButtons = document.querySelectorAll('.site-nav-btn');
+
 const scrollTop = () => {
     console.log('hello');
     document.body.scrollTop = 0;
@@ -41,6 +48,39 @@ const windowScrollEvents = () => {
     }
 };
 
+const openNav = () => {
+    siteNavigation.classList.replace('hide-nav', 'display-nav');
+};
+
+const closeNav = () => {
+    siteNavigation.classList.replace('display-nav', 'hide-nav');
+};
+
+window.addEventListener('load', () => {
+    if (window.matchMedia('(max-width: 800px)').matches) {
+        siteNavigation.classList.add('hide-nav');
+    }
+    if (window.matchMedia('(min-width: 800px)').matches) {
+        siteNavigation.classList.remove('hide-nav');
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.matchMedia('(max-width: 800px)').matches) {
+        siteNavigation.classList.add('hide-nav');
+    }
+    if (window.matchMedia('(min-width: 800px)').matches) {
+        siteNavigation.classList.remove('hide-nav');
+    }
+});
+
 scrollTopBtn.addEventListener('click', scrollTop);
 scrollDownBtn.addEventListener('click', scrollDown);
 window.addEventListener('scroll', windowScrollEvents);
+
+// navigation events
+navOpenBtn.addEventListener('click', openNav);
+navCloseBtn.addEventListener('click', closeNav);
+navigationButtons.forEach((item) => {
+    item.addEventListener('click', closeNav);
+});
