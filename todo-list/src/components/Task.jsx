db8384143ac6df346cd80d1ef3a6badb79c17d74
nimeshakya <nimeshakyabtw@gmail.com>
2023-08-09 19:04:13 +0545

    html warnings dealt with react standards

--- todo-list/src/components/Task.jsx
+++ todo-list/src/components/Task.jsx
@@ -73,15 +73,15 @@ const Task = ({
                 >
                     <svg
                         xmlns='http://www.w3.org/2000/svg'
-                        class='icon icon-tabler icon-tabler-pencil'
+                        className='icon icon-tabler icon-tabler-pencil'
                         width='24'
                         height='24'
                         viewBox='0 0 24 24'
-                        stroke-width='2'
+                        strokeWidth='2'
                         stroke='currentColor'
                         fill='none'
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
                     >
                         <path
                             stroke='none'
@@ -98,15 +98,15 @@ const Task = ({
                 >
                     <svg
                         xmlns='http://www.w3.org/2000/svg'
-                        class='icon icon-tabler icon-tabler-trash'
+                        className='icon icon-tabler icon-tabler-trash'
                         width='24'
                         height='24'
                         viewBox='0 0 24 24'
-                        stroke-width='2'
+                        strokeWidth='2'
                         stroke='currentColor'
                         fill='none'
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
                     >
                         <path
                             stroke='none'
